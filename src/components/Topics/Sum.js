34f9507ab270536b59d9403ac89826043e46251e
Michael Flanders <mflanders06@gmail.com>
2021-03-07 14:51:44 -0700

    half way through sum

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 
 class Sum extends Component{
+    constructor(){
+        super();
+        this.state = {
+            number1: 0,
+            number2: 0,
+            sum: null
+        }
+    }
+    changeHandler(val, select){
+        if (select === 1){
+            this.setState({ number1: val })
+        }
+        else if (select === 2){
+            this.setState({ number2: val })
+        }
+        //console.log(this.state.number1);
+    }
+
+    submitHandler(){
+        
+    }
+
     render () {
         return(
-            <p>Sum</p>
+            <div className="puzzleBox sumPB">
+                <h4>Sum</h4>
+                <input className="inputLine" onChange={ (e) => this.changeHandler(e.target.value, 1) } ></input>
+                <input className="inputLine" onChange={ (e) => this.changeHandler(e.target.value, 2) }></input>
+                <button className="confirmationButton" onClick={ (e) => this.submitHandler(e.target.value) } >Sum</button>
+                <span className="resultsBox"></span>
+            </div>
         )
     }
 }
